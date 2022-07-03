b0d7d623a3d1be30089505fb16b8137b87d41a39
Devan Hurst <devan.hurst@gmail.com>
2022-07-03 13:17:49 -0400

    Add express and endpoint to check if a room exists.

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const http_1 = require("http");
 const socket_io_1 = require("socket.io");
-const dotenv_1 = __importDefault(require("dotenv"));
 const RoomManager_1 = __importDefault(require("./RoomManager"));
-dotenv_1.default.config();
 const port = parseInt(process.env.PORT, 10) || 3000;
-const io = new socket_io_1.Server({
+const app = (0, express_1.default)();
+const options = {
+    origin: "http://localhost:3000",
+};
+app.use((0, cors_1.default)(options));
+app.get('/rooms/:roomCode', (req, res) => {
+    const roomCode = req.params.roomCode;
+    const exists = !!RoomManager_1.default.findRoom(roomCode);
+    res.json({ exists });
+});
+const server = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(server, {
     cors: {
         origin: "*",
-        credentials: false,
     }
 });
 io.on("connection", (socket) => {
@@ -22,4 +34,4 @@ io.on("connection", (socket) => {
     socket.data.roomCode = roomCode;
     RoomManager_1.default.assignRoom(socket);
 });
-io.listen(port);
+server.listen(port);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
+import 'dotenv/config';
+
+import express from "express";
+import cors from "cors";
+import { createServer } from "http";
 import { Server, Socket } from "socket.io";
-import dotenv from 'dotenv';
 import roomManager from "./RoomManager";
 
-dotenv.config();
 const port: number = parseInt(process.env.PORT as string, 10) || 3000;
 
-const io = new Server({
+const app = express();
+
+const options: cors.CorsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(options));
+
+app.get('/rooms/:roomCode', (req, res) => {
+  const roomCode = req.params.roomCode as string;
+  const exists = !!roomManager.findRoom(roomCode);
+  res.json({ exists });
+})
+
+const server = createServer(app);
+
+const io = new Server(server, {
   cors: {
     origin: "*",
-    credentials: false,
   }
 });
 
@@ -23,4 +41,4 @@ io.on("connection", (socket: Socket) => {
   roomManager.assignRoom(socket);
 });
 
-io.listen(port);
+server.listen(port);
