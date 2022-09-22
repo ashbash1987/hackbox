030dbbdb44c221abec429a509554fe46954cb56a
Devan Hurst <devan.hurst@gmail.com>
2022-09-22 15:01:11 -0500

    Add create game button

--- buzz/src/views/HomeView.vue
+++ buzz/src/views/HomeView.vue
@@ -7,7 +7,7 @@
   <main>
     This route will create a game with the settings provided. Then it will
     connect to hackbox. It will then provide the gameId with the room code.
-    <button></button>
+    <button>Create game</button>
   </main>
   <footer>Made with ❤️ by devdev, 2022</footer>
 </template>
