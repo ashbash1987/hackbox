99a95c9b84c90a088c33398b5385261e99b82458
Devan Hurst <devan.hurst@gmail.com>
2023-10-02 15:51:08 -0400

    Add docusaurus app for documentation

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,97 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Hackbox",
+  tagline: "Open-source framework for building Jackbox-style games",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://docs.hackbox.ca",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "devanhurst", // Usually your GitHub org/user name.
+  projectName: "hackbox", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: "https://github.com/devanhurst/hackbox/tree/main/docs/",
+        },
+        blog: false,
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Hackbox",
+        logo: {
+          alt: "Hackbox Logo",
+          src: "img/logo.png",
+        },
+        items: [
+          {
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Docs",
+          },
+          {
+            href: "https://sandbox.hackbox.ca",
+            label: "Sandbox",
+            position: "left",
+          },
+          {
+            href: "https://github.com/devanhurst/hackbox",
+            label: "GitHub",
+            position: "right",
+          },
+          {
+            href: "https://discord.gg/Y9bAfgaKZc",
+            label: "Discord",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        copyright: `Hackbox © ${new Date().getFullYear()}`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
